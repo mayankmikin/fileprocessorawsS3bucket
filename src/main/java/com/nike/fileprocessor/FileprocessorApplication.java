c60086b9062c398fd54405e4bb4cff1d8fa8ded4
MayankVerma <mayank.verma@nviz.com>
2018-06-20 22:40:24 +0530

    file upload into s3 bucket  with no max restriction on file size

---
+++ src/main/java/com/nike/fileprocessor/FileprocessorApplication.java
@@ -0,0 +1,12 @@
+package com.nike.fileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileprocessorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nike/fileprocessor/FileprocessorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nike.fileprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileprocessorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
