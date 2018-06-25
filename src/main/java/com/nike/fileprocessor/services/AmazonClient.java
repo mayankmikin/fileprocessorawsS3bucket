606fc81c0c32264bdc9e6c010417f9573a8445b3
MayankVerma <mayank.verma@nviz.com>
2018-06-25 21:30:41 +0530

    added s3 mock successfully and tested with file upload

--- src/main/java/com/nike/fileprocessor/services/AmazonClient.java
+++ src/main/java/com/nike/fileprocessor/services/AmazonClient.java
@@ -12,13 +12,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
+import io.findify.s3mock.S3Mock;
 @Service
 public class AmazonClient {
 
@@ -32,11 +37,36 @@ public class AmazonClient {
     private String accessKey;
     @Value("${amazonProperties.secretKey}")
     private String secretKey;
-
+    @Value("${amazonProperties.directorypathtomock}")
+    private String directory;
+    
     @PostConstruct
     private void initializeAmazon() {
         AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-        this.s3client = new AmazonS3Client(credentials);
+        //this.s3client = new AmazonS3Client(credentials);
+       // S3Mock api = new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
+        S3Mock api = S3Mock.create(8001, directory);
+        api.start();
+                	
+        /* AWS S3 client setup.
+         *  withPathStyleAccessEnabled(true) trick is required to overcome S3 default 
+         *  DNS-based bucket access scheme
+         *  resulting in attempts to connect to addresses like "bucketname.localhost"
+         *  which requires specific DNS setup.
+         */
+        EndpointConfiguration endpoint = new EndpointConfiguration("http://localhost:8001", "us-west-2");
+        AmazonS3 client = AmazonS3ClientBuilder
+          .standard()
+          .withPathStyleAccessEnabled(true)  
+          .withEndpointConfiguration(endpoint)
+          .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+          .build();
+
+        client.createBucket(bucketName);
+       // client.putObject(bucketName, "file/name", "contents");
+        api.stop();
+        this.s3client=client;
+        
     }
 
     public String uploadFile(MultipartFile multipartFile) {
